1c3206c8cc48d30b10d193169b0708804a4796e5
AsrafulAsif <asifasrafulislam@gmail.com>
2024-09-23 00:30:29 +0600

    login added

--- login_page.js
+++ login_page.js
@@ -1,3 +1,42 @@
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById('submitBtn').addEventListener('click', () => {
+        const userName = document.getElementById('userName').value;
+        const userPassword = document.getElementById('userPassword').value;
+        if (userName == '' || userPassword == '') {
+            alert('Enter user name or password.')
+        } else {
+            loginUser(userName, userPassword);
+        }
+    });
+
+    function loginUser(userName, userPassword) {
+        const url = 'http://localhost:8080/api/v1/auth/login';
+
+        const data = {
+            userName: userName,
+            password: userPassword
+        };
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
 
 }); 
